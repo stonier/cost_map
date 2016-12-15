337136e427aa9590f90042e6268854f879733ccf
Daniel Stonier <d.stonier@gmail.com>
2016-12-15 10:50:51 +0900

    Revamped image bundle loader (#33)
    * [cost_map_ros] refactor cost_map->cost_map_ros for resource exports
    
    * [cost_map] revamped the image bundle loader

---
+++ cost_map_ros/src/applications/publish_image_bundle.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file src/applications/publish_image_bundle.cpp
+ *
+ * Grabs an image bundle, loads it into a costmap and publishes it.
+ */
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <ecl/command_line.hpp>
+#include <ecl/console.hpp>
+#include <ecl/exceptions.hpp>
+
+#include <ros/init.h>
+#include <ros/ros.h>
+#include <string>
+
+#include "../../include/cost_map_ros/loader.hpp"
+#include "../../include/cost_map_ros/utilities.hpp"
+
+/*****************************************************************************
+** Main
+*****************************************************************************/
+
+int main(int argc, char **argv) {
+  ecl::CmdLine cmd("Load an image bundle into a costmap and publish");
+  ecl::ValueArg<std::string> topicNameArg(
+      "t",
+      "topic_name",
+      "Where to publish [default: ~cost_map]",
+      false,
+      "cost_map",
+      "string");
+  ecl::UnlabeledValueArg<std::string> imageBundleArg(
+      "image_bundle_location",
+      "A yaml file with meta-data about the costmap to be built",
+      true,
+      "cost_map_ros/example",
+      "string (package/name OR filename)");
+  cmd.add(topicNameArg);
+  cmd.add(imageBundleArg);
+  std::vector<std::string> myargs;
+  ros::removeROSArgs(argc, argv, myargs);
+  char** myargv = new char*[myargs.size()]; //array to emulate argv.
+  for(int i = 0, ie = myargs.size(); i < ie; ++i)
+  {
+    myargv[i] = const_cast<char*>(myargs[i].data());
+  }
+  cmd.parse(myargs.size(), myargv);
+
+
+  ros::init(argc, argv, "image_bundle");
+  try {
+    cost_map::Loader loader(imageBundleArg.getValue(), topicNameArg.getValue());
+//    if ( broken_layer_probably_isnt_obstacle_costs ) {
+//      cost_map::Matrix data = loader.cost_map->get("obstacle_costs");
+//      for (int i = 0, number_of_rows = data.rows(), number_of_columns = data.cols(); i < number_of_rows; ++i) {
+//        for (int j = 0; j < number_of_columns; ++j) {
+//          std::cout << static_cast<int>(data(i,j)) << " ";
+//        }
+//        std::cout << std::endl;
+//      }
+//    }
+    ros::spin();
+  } catch (const ecl::StandardException& e) {
+    std::cout << ecl::red << "[ERROR] " << e.what() << ecl::reset << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
