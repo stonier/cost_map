30b7114618e42af651957d8ee54cf21751a6dab3
Daniel Stonier <d.stonier@gmail.com>
2016-12-15 14:47:05 +0900

    [cost_map_ros] image bundle command line utilities

--- cost_map_ros/src/lib/image_bundles.cpp
+++ cost_map_ros/src/lib/image_bundles.cpp
@@ -7,7 +7,7 @@
 
 #include <cost_map_msgs/CostMap.h>
 #include "../../include/cost_map_ros/converter.hpp"
-#include "../../include/cost_map_ros/loader.hpp"
+#include "../../include/cost_map_ros/image_bundles.hpp"
 #include "../../include/cost_map_ros/utilities.hpp"
 
 /*****************************************************************************
@@ -20,8 +20,9 @@ namespace cost_map {
 ** Implementation
 *****************************************************************************/
 
-Loader::Loader(const std::string& image_bundle_location,
-               const std::string& topic_name)
+LoadImageBundle::LoadImageBundle(
+    const std::string& image_bundle_location,
+    const std::string& topic_name)
 {
   ros::NodeHandle nodehandle("~");
   publisher = nodehandle.advertise<cost_map_msgs::CostMap>(topic_name, 1, true);
@@ -34,11 +35,38 @@ Loader::Loader(const std::string& image_bundle_location,
   // toImageBundle("debug.yaml", *cost_map);
 }
 
-void Loader::publish() {
+void LoadImageBundle::publish() {
   cost_map_msgs::CostMap map_message;
   cost_map::toMessage(*cost_map, map_message);
   publisher.publish(map_message);
 }
+
+SaveImageBundle::SaveImageBundle(
+    const std::string& topic_name,
+    const std::string& yaml_filename)
+: finished(false)
+, yaml_filename(yaml_filename)
+{
+  ros::NodeHandle nodehandle("~");
+  // TODO : check that a publisher exists and warn if not available?
+  subscriber_ = nodehandle.subscribe(topic_name, 1, &SaveImageBundle::costmapCallback, this);
+}
+
+void SaveImageBundle::costmapCallback(const cost_map_msgs::CostMap& msg) {
+  std::lock_guard<std::mutex> guard(mutex_);
+  if ( !finished ) {
+    cost_map::CostMap cost_map;
+    if ( !fromMessage(msg, cost_map) ) {
+      ROS_ERROR_STREAM("SaveImageBundle : failed to convert cost map msg -> cost map class");
+      return;
+    }
+    toImageBundle(yaml_filename, cost_map);
+    ROS_INFO_STREAM("SaveImageBundle : successfully saved to '" << yaml_filename << "'");
+    finished = true;
+  }
+}
+
+
 
 /*****************************************************************************
  ** Trailers
